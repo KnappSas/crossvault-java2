5c0d83128c9ae7f43abb946d293bbe190c84d6ff
paulsoeldner <paulsoeldner@users.noreply.github.com>
2017-02-23 14:20:09 +0100

    Update Controller.java
    An GUI angepasst

--- Regression/src/Regression/Controller.java
+++ Regression/src/Regression/Controller.java
@@ -1,27 +1,39 @@
 package Regression;
 
 
-import GUI.Plot;
 import IO.Input;
+import IO.Output;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import GUI.Plot;
+import GUI.TestGUI;
 
 public class Controller
 {
     static Double[] x = {0.0, 2.0, 4.0, 6.0, 8.0, 9.0, 1.0, 2.0, 4.0, 6.0, 8.0, 9.0, 2.0, 4.0, 6.0, 8.0, 9.0, 3.0, 4.0, 6.0, 8.0, 9.0, 4.0, 6.0, 8.0, 9.0, 5.0, 6.0, 8.0, 6.0, 8.0, 7.0};
     static Double[] y = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 2.0, 2.0, 3.0, 3.0, 3.0, 3.0, 3.0, 4.0, 4.0, 4.0, 4.0, 5.0, 5.0, 5.0, 6.0, 6.0, 7.0};
     static Double[] z = {150.0, 146.0, 134.0, 114.0, 86.0, 69.0, 148.0, 145.0, 133.0, 113.0, 85.0, 68.0, 142.0, 130.0, 110.0, 82.0, 65.0, 132.0, 125.0, 105.0, 77.0, 60.0, 118.0, 98.0, 70.0, 53.0, 100.0, 89.0, 61.0, 78.0, 50.0, 52.0};
-
-//    static Double[] x = {-1d, 0d, 1d, 2d, 3d, 5d, 7d, 9d};
-//    static Double[] y = {-1d, 3d, 2.5, 5d, 4d, 2d, 5d, 4d};
-
-    public static void main(String[] args)
+    private static double genauigkeit = 0.01;
+    
+    public static void main(String[] args) throws IOException
     {
-        ArrayList<ArrayList<Point3D>> p = null;
+
+        TestGUI test = new TestGUI();
+        test.gui();
+
+        
+    }
+    
+    public static void update(File file, double genauigkeit2, TestGUI testgui) {
+    	genauigkeit = genauigkeit2;
+    	
+    	ArrayList<ArrayList<Point3D>> p = null;
         try
         {
-            p = Input.readFromFile("/home/killer/Documents/Kreuzgewölbe-Projekt/Regression/test/input.txt");
+            p = Input.readFromFile(file.getAbsolutePath());
         }
         catch (IOException e)
         {
@@ -30,15 +42,7 @@ public class Controller
 
         PolynomePool pool = new PolynomePool();
         pool.approximate(p);
-        Plot.gui(pool.getXPolynomes(), pool.getYPolynomes(), 1);
-
-//        ArrayList<Point2D> p = new ArrayList<Point2D>();
-//        for(int i = 0; i < x.length; i++)
-//        {
-//            p.add(new Point2D(x[i], y[i]));
-//        }
-//
-//        Regression r = new Regression();
-//        r.approximate(p, 8);
+        
+        testgui.update(pool.getXPolynomes(), pool.getYPolynomes(), genauigkeit);
     }
 }
